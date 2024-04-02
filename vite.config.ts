5a3a3de800cc4ecf762a729a50f5dd3f4019e22d
alibi-jia <alibi_jia@foxmail.com>
2024-04-02 12:29:28 +0800

    调整首页

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/threejs_on_worker/',
   resolve: {
     alias: {
       'R': resolve(__dirname, './src')
